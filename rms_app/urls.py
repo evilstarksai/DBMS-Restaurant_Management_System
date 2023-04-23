776d036fcde1f287ffb97a6429e56c859019f96c
evilstarksai <krishnasaivangala2311@gmail.com>
2023-04-24 01:48:16 +0530

    Making the Repo

---
+++ rms_app/urls.py
@@ -0,0 +1,46 @@
+from django.urls import path
+from . import views
+from .views import ( 
+    FoodListView,
+    AddToListView,
+    UpdateListView,
+    DeleteItemListView,
+    FoodImgView,
+    FoodImgViewStarter,
+    FoodImgViewMainCourse,
+    FoodImgViewDesserts,
+    FoodImgViewBeverages,
+    CreateInvoiceView,
+    DeleteInvoiceView,
+    SalesView,
+    AddItemsToInvoiceView,
+    InvoiceDetailView,
+    GeneratePdf,
+    )
+
+
+
+urlpatterns = [
+    
+    #URLS FOR HOME AND FOODITEMS
+    path("", views.home, name="rms-home"),
+    path("food/", FoodImgView.as_view(), name="rms-food"),
+    path("food/all", FoodImgView.as_view(), name="rms-food"),
+    path("food/starters", FoodImgViewStarter.as_view(), name="rms-food_starters"),
+    path("food/maincourse", FoodImgViewMainCourse.as_view(), name="rms-food_maincourse"),
+    path("food/desserts", FoodImgViewDesserts.as_view(), name="rms-food_desserts"),
+    path("food/beverages", FoodImgViewBeverages.as_view(), name="rms-food_beverages"),
+    path("foodlist/", FoodListView.as_view(), name="rms-foodlist"),
+    path('dashboard/addtolist',AddToListView.as_view(),name="rms-addtolist"),
+    path('foodlist/<int:pk>/delete/',DeleteItemListView.as_view(),name="food_confirm_delete"),
+    path('dashboard/addtolist/<int:pk>/update/',UpdateListView.as_view(),name="rms-updatetolist"),
+
+    #URLS FOR INVOICES
+    path('dashboard/createinvoice', CreateInvoiceView.as_view(), name='createinvoice'),
+    path("dashboard/sales", SalesView.as_view(), name="sales"),
+    path("invoices/<int:pk>", AddItemsToInvoiceView.as_view(), name="add_items_on_invoice"),
+    path("invoices/<int:pk>/details", InvoiceDetailView.as_view(), name="invoice_details"),
+    path('dashboard/sales/<int:pk>/delete/',DeleteInvoiceView.as_view(),name="invoice_confirm_delete"),
+    path('invoices/<int:pk>/details/print', GeneratePdf.as_view(), name="invoice_print"),
+
+]
---
+++ rms_app/views.py
@@ -0,0 +1,210 @@
+from django.shortcuts import render
+from django.db.models import Q
+from django.views.generic.base import TemplateView
+from .models import Food ,Invoice
+from django.contrib import messages
+from crispy_forms.helper import FormHelper
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.urls import reverse
+from .forms import  InvoiceFormSet
+from django.views.generic.detail import SingleObjectMixin
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic import (
+    ListView,
+    TemplateView,
+    CreateView,
+    FormView,
+    UpdateView,
+    DeleteView,
+)
+from .utils import render_to_pdf
+
+
+
+
+def home(request):
+    return render(request,"rms_app/home.html")
+
+
+
+
+
+#FOOD_VIEWS
+class FoodListView(ListView):
+    model = Food
+    template_name = "rms_app/foodlist.html"  # by default - app/model_viewtype.html
+    context_object_name = "food_obj"
+
+
+class FoodImgView(ListView):
+    model = Food
+    template_name = "rms_app/food.html"  
+    context_object_name = "food_obj"
+
+class FoodImgViewStarter(ListView):
+    model = Food
+    template_name = "rms_app/food_starters.html"  
+    context_object_name = "food_obj"
+
+class FoodImgViewMainCourse(ListView):
+    model = Food
+    template_name = "rms_app/food_maincourse.html"  
+    context_object_name = "food_obj"
+
+class FoodImgViewDesserts(ListView):
+    model = Food
+    template_name = "rms_app/food_desserts.html"  
+    context_object_name = "food_obj"
+
+class FoodImgViewBeverages(ListView):
+    model = Food
+    template_name = "rms_app/food_beverages.html"  
+    context_object_name = "food_obj"
+
+
+class AddToListView(LoginRequiredMixin, CreateView):
+    model = Food
+    fields = ["food_item", "per_price","food_type","food_availability","food_img"]
+    template_name = "rms_app/addtolist.html"
+    success_url = "/dashboard/addtolist"
+
+    def form_valid(self,form,):
+        messages.success(self.request, f'Foodlist updated!')
+        return super().form_valid(form)
+
+class UpdateListView(LoginRequiredMixin, UpdateView):
+    model = Food
+    fields = ["food_item", "per_price","food_type","food_availability","food_img"]
+    template_name = "rms_app/addtolist.html"
+    success_url = "/food"
+    
+    def form_valid(self,form):
+        messages.success(self.request, f'Foodlist updated!')
+        return super().form_valid(form)
+
+
+class DeleteItemListView(LoginRequiredMixin,DeleteView):
+    model = Food
+    success_url= '/foodlist'
+
+
+
+#INVOICE_VIEWS
+class CreateInvoiceView(LoginRequiredMixin, CreateView):
+    model = Invoice
+    fields = ["customer_name"]
+    context_object_name = 'invoice_obj'
+    template_name = "rms_app/create_invoice.html"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'CurrentProfile'
+
+    def form_valid(self,form):
+        messages.success(self.request, f'Invoice Created!')
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('add_items_on_invoice', kwargs={'pk': self.object.pk})
+
+
+
+class AdminRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class DeleteInvoiceView(AdminRequiredMixin,DeleteView):
+    model = Invoice
+    success_url = '/dashboard/sales'
+
+
+class AddItemsToInvoiceView(LoginRequiredMixin,SingleObjectMixin,FormView,TemplateView):
+    model = Invoice
+    context_object_name = 'invoice_obj'
+    template_name = 'rms_app/additemsoninvoice.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Invoice.objects.all())
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Invoice.objects.all())
+        return super().post(request, *args, **kwargs)
+
+    def get_form(self, form_class=None):
+        return InvoiceFormSet(**self.get_form_kwargs(), instance=self.object)
+
+    def form_valid(self, form):
+        form.save()
+        messages.add_message(
+            self.request,
+            messages.SUCCESS,
+            'Changes were saved.'
+        )
+        return HttpResponseRedirect(self.get_success_url())
+          
+    def get_success_url(self):
+        return reverse('add_items_on_invoice', kwargs={'pk': self.object.pk})
+
+
+class InvoiceDetailView(LoginRequiredMixin,SingleObjectMixin,TemplateView):
+
+    model = Invoice
+    context_object_name = 'invoice_obj'
+    template_name = 'rms_app/invoicedetails.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Invoice.objects.all())
+        return super().get(request, *args, **kwargs)
+          
+    def get_success_url(self):
+        return reverse('invoice_details', kwargs={'pk': self.object.pk})
+
+
+
+class SalesView(LoginRequiredMixin, ListView):
+    model = Invoice
+    context_object_name = 'invoice_obj'
+    template_name = "rms_app/sales.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get('q',None)
+        query2 = self.request.GET.get('d',None)
+        if query is not None :
+            invoice_obj = self.model.objects.filter( Q(customer_name__icontains = query) | Q(created_by__username__icontains = query)
+            )
+        elif query2 is not None :
+            invoice_obj = self.model.objects.filter( Q(date_created__year = query2) | Q(date_created__month = query2) | Q(date_created__day = query2)
+            )
+        else:
+            invoice_obj = self.model.objects.all()
+        return invoice_obj
+
+
+
+class GeneratePdf(LoginRequiredMixin,View,SingleObjectMixin):
+    model = Invoice
+    context_object_name = 'invoice_obj'
+
+    def get(self, request, pk, *args, **kwargs):
+        self.object = self.get_object(queryset=Invoice.objects.all()) 
+        total = 0
+        for itr in self.object.invoiceitem.all():
+            total += itr.accumulated
+
+        params = {
+            'invoice_obj': self.object,
+            'request': request,
+            'total': total
+        }
+        pdf = render_to_pdf('rms_app/invoice_print.html', params)
+        return HttpResponse(pdf, content_type='application/pdf')
+
+
+
